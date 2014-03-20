e0ba730a0fd218804aecef50b4a1dc5c45d03e69
Toby Dylan Hocking <tdhock5@gmail.com>
2014-03-20 11:20:45 -0400

    rows onscreen seconds columns

--- table-examples.R
+++ table-examples.R
@@ -1,10 +1,9 @@
-works_with_R("3.0.2", animint="2014.3.19", xtable="1.7.3",
-             microbenchmark="1.3.0")
+works_with_R("3.0.2", animint="2014.3.19", xtable="1.7.3")
 viz.path <- Sys.glob(file.path("examples", "*", "viz.R"))
 sub.dirs <- dirname(viz.path)
 base <- basename(sub.dirs)
 n.vars <- n.interactive <- name.vars <- name.interactive <- code.lines <-
-  comment.lines <- 
+  comment.lines <- n.plots <- n.rows <- n.onscreen <- seconds <- 
   rep(NA, length(base))
 animated <- rep("no", length(base))
 for(sub.dir.i in seq_along(sub.dirs)){
@@ -12,11 +11,38 @@ for(sub.dir.i in seq_along(sub.dirs)){
   cat(sprintf("%4d / %4d %s\n", sub.dir.i, length(sub.dirs), sub.dir))
   viz.f <- file.path(sub.dir, "viz.R")
   source(viz.f)
-  ## TODO: microbenchmark this!!
-  meta <- gg2animint(viz, open.browser=FALSE)
+  n.plots.i <- 0
+  n.onscreen.i <- 0
+  n.rows.i <- 0
+  for(L in viz){
+    if(is.ggplot(L)){
+      n.plots.i <- n.plots.i + 1
+      for(l in L$layers){
+        if(!is.null(nrow(l$data))){
+          n.rows.i <- n.rows.i + nrow(l$data)
+          aes.names <- names(l$mapping)
+          is.ss <- grepl("showSelected", aes.names)
+          chunk.rows <- if(any(is.ss)){
+            split.by <- l$data[as.character(l$mapping[is.ss])]
+            do.call(table, split.by)
+          }else{
+            nrow(l$data)
+          }
+          n.onscreen.i <- n.onscreen.i + median(chunk.rows)
+        }
+      }
+    }
+  }
+  stopifnot(n.onscreen.i < n.rows.i)
+  n.onscreen[[sub.dir.i]] <- n.onscreen.i
+  n.rows[[sub.dir.i]] <- n.rows.i
+  n.plots[[sub.dir.i]] <- n.plots.i
+  seconds[[sub.dir.i]] <- system.time({
+    meta <- gg2animint(viz, open.browser=FALSE)
+  })[["elapsed"]]
   vizLines <- readLines(viz.f)
   no.comments <- sub("#.*", "", vizLines)
-  no.whitespace <- sub(" ", "", no.comments)
+  no.whitespace <- gsub(" ", "", no.comments)
   no.blank <- no.whitespace[no.whitespace != ""]
   comment.lines[[sub.dir.i]] <- length(vizLines)-length(no.blank)
   code.lines[[sub.dir.i]] <- length(no.blank)
@@ -41,16 +67,21 @@ for(sub.dir.i in seq_along(sub.dirs)){
     animated[[sub.dir.i]] <- "yes"
   }
 }
-tab <- data.frame(data=base,
-                  vars=n.vars,
-                  int=n.interactive,
-                  anim=animated,
-                  LOC=code.lines,
+tab <- data.frame(LOC=code.lines,
+                  plots=as.integer(n.plots),
+                  rows=as.integer(n.rows),
+                  onscreen=as.integer(n.onscreen),
+                  ##"rows/onscreen"=n.rows/n.onscreen,
+                  variables=n.vars,
+                  interactive=n.interactive,
+                  "animated?"=animated,
+                  seconds=round(seconds, 1),
                   ##comments=comment.lines,
+                  check.names=FALSE,
                   row.names=base)
 tab <- tab[order(tab$LOC, decreasing=FALSE),]
-tab$figure <- NA
-tab$seconds <- NA
-tab[c("WorldBank", "tornado", "climate"), "figure"] <- c(1L, 2L, 3L)
-xt <- xtable(tab)
-print(xt, file="table-examples.tex", include.rownames=FALSE, floating=FALSE)
+tab$Fig <- NA
+tab[c("WorldBank", "tornado", "climate"), "Fig"] <- c(1L, 2L, 3L)
+print(tab)
+xt <- xtable(tab, digits=1)
+print(xt, file="table-examples.tex", floating=FALSE)
